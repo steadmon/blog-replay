34b0f49c9e34f56d2ed0471a580c42210fa89aae
Josh Steadmon <josh@steadmon.net>
2022-03-04 21:38:01 -0800

    Set up Nix flake.
    Use cargo2nix and their example at
    https://github.com/cargo2nix/cargo2nix/tree/master/examples/1-hello-world

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=release-21.05";
+    cargo2nix.url = "github:cargo2nix/cargo2nix/master";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
+    rust-overlay.inputs.flake-utils.follows = "flake-utils";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, cargo2nix, flake-utils, rust-overlay, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [(import "${cargo2nix}/overlay")
+                      rust-overlay.overlay];
+        };
+
+        rustPkgs = pkgs.rustBuilder.makePackageSet' {
+          rustChannel = "1.58.1";
+          packageFun = import ./Cargo.nix;
+        };
+
+      in rec {
+        packages = {
+          feed-replay = (rustPkgs.workspace.feed-replay {}).bin;
+        };
+
+        defaultPackage = packages.feed-replay;
+      }
+    );
+}
