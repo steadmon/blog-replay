53a16dbcc9eea589c1f5682455efe8ee66a960fd
Josh Steadmon <josh@steadmon.net>
2024-03-07 22:09:30 -0800

    Add metadata to Nix package
    Fixes #10.

--- default.nix
+++ default.nix
@@ -7,5 +7,15 @@ in {
     src = ./.;
     nativeBuildInputs = [ pkgs.pkg-config ];
     PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
+    meta = with pkgs.lib; {
+      description = "A utility to replay a blog's archive into an Atom feed";
+      longDescription = ''
+        blog-replay scrapes articles from a given blog and stores them in a local Sled database. It
+        can then gradually replay these articles into an Atom feed, which can be hosted on a
+        website or consumed by a local feed reader.
+      '';
+      homepage = "https://github.com/steadmon/blog-replay";
+      license = licenses.mit;
+    };
   };
 }
